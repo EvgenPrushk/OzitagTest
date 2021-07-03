b91e222ada3cf7a478f8fb421750f0dcedc92685
EvgenPrushk <ogmprushak@mail.ru>
2021-07-04 01:42:38 +0300

    feat: add client

--- ozitag/routes/auth.route.js
+++ ozitag/routes/auth.route.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const bcrypt = require("bcrypt");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
 const User = require("../models/User");
 const router = Router();
@@ -17,7 +19,8 @@ router.post(
 
       if (!errors.isEmpty()) {
         return res.status(400).json({
-            
+          errors: errors.array(),
+          message: "Incorrect registration data ",
         });
       }
       //add field
@@ -45,6 +48,45 @@ router.post(
 );
 
 // /api/auth/login
-router.post("/login", async (req, res) => {});
+router.post(
+  "/login",
+  [
+    check("email", "enter correct email").normalizeEmail().isEmail(),
+    check("password", "enter password").exists(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: "Incorrect login data ",
+        });
+      }
+
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ message: "User not found" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ message: "Incorrect password" });
+      }
+
+      // create token
+      const token = jwt.sign({ userId: user.id }, config.get("jwtSecret"), {
+        expiresIn: "1h",
+      });
+      res.json({ token, userId: user.id })
+    } catch (e) {
+      res.status(500).json({ message: "Reped please" });
+    }
+  }
+);
 
 module.exports = router;
